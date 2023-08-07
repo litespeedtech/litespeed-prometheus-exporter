65fd002152e9bae951e84287ba659681c1b87610
Robert Perper <rperper@litespeedtech.com>
2023-08-07 16:06:28 -0400

    Version 0.0.1

--- mkdist.sh
+++ mkdist.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -x
 echo Create distribution tar
+git rm lsws-prometheus-exporter.*
 tar czfv lsws-prometheus-exporter.$VERSION.tgz dist --transform s/dist/lsws-prometheus-exporter/
+git add lsws-prometheus-exporter.*
+git commit -m "Version $VERSION"
+git push litespeed-prometheus-exporter
