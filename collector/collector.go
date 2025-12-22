6f3838d9a1b5bad7fa68dec0f351a05cde361be8
Bob Perper <rperper@litespeedtech.com>
2025-12-22 15:39:06 -0500

    Version 0.1.3

--- collector/collector.go
+++ collector/collector.go
@@ -67,13 +67,12 @@ func customMetricsHandler() http.Handler {
 		userAgent := r.Header.Get("User-Agent")
 		authorization := r.Header.Get("Authorization")
 		klog.V(4).Infof("Metrics request received with User-Agent: %s Authorization: %s\n", userAgent, authorization)
-		if authorization != "" {
-			decodedBytes, err := base64.StdEncoding.DecodeString(authorization)
-			if err != nil {
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			} else {
-				klog.V(4).Infof("Decoded Authorization: %v\n", decodedBytes)
-			}
+		decodedBytes, err := base64.StdEncoding.DecodeString(authorization)
+		if err != nil {
+			klog.Errorf("Could not decode authorization")
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		} else {
+			klog.V(4).Infof("Decoded Authorization: %v\n", decodedBytes)
 		}
 
 		// You can also set response headers here
