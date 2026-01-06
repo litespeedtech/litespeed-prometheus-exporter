cb2d36cfc85e1a5119bb903206bac0962e012289
Bob Perper <rperper@litespeedtech.com>
2026-01-06 14:49:36 -0500

    Version 0.1.4

--- collector/collector.go
+++ collector/collector.go
@@ -399,7 +399,7 @@ func (c *LitespeedCollector) scrapeFile(fileName string) (report *litespeedRepor
 	}()
 
 	idRegex := regexp.MustCompile(`^\w*`)
-	ibRegex := regexp.MustCompile(`\[(?:\[??[^\[]*?)\]`)
+	ibRegex := regexp.MustCompile(`\[([^\[\]]*)\]`)
 
 	report = &litespeedReport{
 		GeneralInfo: generalInfoReport{KeyValues: make(map[string]float64)},
@@ -445,12 +445,12 @@ func (c *LitespeedCollector) scrapeFile(fileName string) (report *litespeedRepor
 			}
 		case reqRateField:
 			parts := strings.SplitN(line, ": ", 2)
-			//matches := ibRegex.FindStringSubmatch(line)
+			matches := ibRegex.FindStringSubmatch(line)
 
 			m := parseKeyValLineToMap(parts[1])
 			rr := requestRateReport{
-				//VHost:     matches[1],
-				VHost:     captureOutermostBrackets(line),
+				VHost: matches[1],
+				//VHost:     captureOutermostBrackets(line),
 				KeyValues: make(map[string]float64),
 			}
 			klog.V(4).Infof("reqRate report, vhost: %v kvs: %v", rr.VHost, m)
